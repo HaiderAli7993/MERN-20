5c0b42ec311bcac25597d9e46667f70d713b378a
haiderali7993 <haider7993@gmail.com>
2024-07-09 22:27:42 -0700

    Modify example.txt in branch-a

---
+++ main.js
@@ -0,0 +1,18 @@
+const a = (num) => {
+    if (num % 2 === 0) {
+        return `${num} is even`;
+    } else {
+        return `${num} is odd`;
+    }
+};
+
+
+
+const evenOdd = (num) => {
+    if (num % 2 === 0) {
+        return `${num} is even`;
+    } else {
+        return `${num} is odd`;
+    }
+};
+
