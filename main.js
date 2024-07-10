2275da2fef92be20faf48df9cc0c9bbc206533f9
haiderali7993 <haider7993@gmail.com>
2024-07-09 23:25:38 -0700

    home feature

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
+console.log("Welcome to home");  // Output: Welcome to JavaScript!
+let ae = 25
+
+
+
 console.log("Welcome to JavaScript!");  // Output: Welcome to JavaScript!
 let age = 25;  // Variable assignment
