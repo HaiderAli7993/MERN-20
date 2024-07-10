6f6d441e14a51c097650018032b5eafeafe5476a
haiderali7993 <haider7993@gmail.com>
2024-07-09 22:50:32 -0700

    add new feature

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Welcome to JavaScript!");  // Output: Welcome to JavaScript!
+let age = 25;  // Variable assignment
+
+
+
+
+console.log("Hello, World!");  // Output: Hello, World!
+let num = 10;  // Variable assignment
