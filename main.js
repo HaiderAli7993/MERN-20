2a34f781edfa154f06dbc3de13ec7f9d6ce00155
haiderali7993 <haider7993@gmail.com>
2024-07-09 23:31:50 -0700

    add office

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
+console.log("Welcome to office");  // Output: Welcome to JavaScript!
+let age = 25
+
+
 console.log("Welcome to JavaScript!");  // Output: Welcome to JavaScript!
-let age = 25;  // Variable assignment
+let a = 25;  // Variable assignment
